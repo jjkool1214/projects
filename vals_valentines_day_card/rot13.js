fe35328ae312014a8b9ced7cbb5bb0c09f9f087f
Jaysen <jaysenstolarz@gmail.com>
2024-02-01 22:03:00 -0500

    grah

--- vals_valentines_day_card/rot13.js
+++ vals_valentines_day_card/rot13.js
@@ -1,5 +1,8 @@
-document.addEventListener("keyup" (e) => {
-    
+document.addEventListener("keyup", (e) => {
+    if("KeyA" <= e.code && e.code <= "KeyZ"){
+        let input = document.getElementById("word-holder-input")
+        input.innerText += e.code[3]
+    }
 })
 
 function rotateLetter(letter, rotation){
