5356bb562c222570c8deb8fc64142f2ea583e76c
Jaysen <jaysenstolarz@gmail.com>
2024-02-02 19:01:46 -0500

    added an input box

--- vals_valentines_day_card/rot13.js
+++ vals_valentines_day_card/rot13.js
@@ -1,9 +1,4 @@
-document.addEventListener("keyup", (e) => {
-    if("KeyA" <= e.code && e.code <= "KeyZ"){
-        let input = document.getElementById("word-holder-input")
-        input.innerText += e.code[3]
-    }
-})
+
 
 function rotateLetter(letter, rotation){
 
