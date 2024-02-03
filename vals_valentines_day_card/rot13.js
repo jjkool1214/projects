df2ca75ab47baf47a82c121365091c7c10124833
Jaysen <jaysenstolarz@gmail.com>
2024-02-02 21:36:33 -0500

    done for now

--- vals_valentines_day_card/rot13.js
+++ vals_valentines_day_card/rot13.js
@@ -1,5 +1,27 @@
-
-
-function rotateLetter(letter, rotation){
-
+function rotateLetter(rotation){
+    let input = document.getElementById("actual-input-text-area").innerText
+    let results = new Array(input.length)
+    for(let i = 0; i < input.length; i++){
+        let charVal = input.charCodeAt(i) + rotation
+        if(charVal + rotation > 90 && charVal < 91){
+            charVal -= 26
+            results.push(charVal)
+        } else {
+            results.push(charVal)
+        }
+    }
+    console.log(results)
+    let showOutput = document.getElementById("results-print")
+    showOutput.innerText = String.fromCharCode(results)
 }
+
+
+
+document.addEventListener("click", (e) => {
+    let currentVal = 1;
+    if(e.target.value != currentVal){
+        currentVal = e.target.value
+        console.log(currentVal)
+        rotateLetter(currentVal)
+    }w
+})
